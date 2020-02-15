a99770c8d0715e0b812d3b6fd042afdc383d2a05
gaojb <xiaomucool@bingblue.com>
2020-02-15 20:46:51 +0800

    修改stylus calc

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -88,7 +88,7 @@ export default {
     top: 0;
     right: 0;
     z-index: 9;
-    width: calc(100% - $sideBarWidth);
+    width: 'calc(100% - %s)' % $sideBarWidth;
     transition: width 0.28s;
   }
 
