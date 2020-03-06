57e811bdd89d483002565aadb2f9580389224e55
gavin.gao <gaojb@mti-sh.cn>
2020-03-06 17:29:21 +0800

    构建：初始化项目

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import 'element-ui/lib/theme-chalk/index.css'
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="stylus">
+body
+  padding-top: 80px
+</style>
